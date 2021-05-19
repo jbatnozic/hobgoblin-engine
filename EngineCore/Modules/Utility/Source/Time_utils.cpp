73750fc7b8c4ca3d90583401cad9490bf511eba4
jbatnozic <jovanbatnozic@hotmail.rs>
2021-05-20 00:05:59 +0200

    Fix precise sleep

--- EngineCore/Modules/Utility/Source/Time_utils.cpp
+++ EngineCore/Modules/Utility/Source/Time_utils.cpp
@@ -1,12 +1,17 @@
 
 #include <Hobgoblin/Utility/Time_utils.hpp>
 
+#include <Hobgoblin/Common.hpp>
+#include <Hobgoblin/Utility/Semaphore.hpp>
+
+#include <cassert>
 #include <cstdint>
 #include <thread>
 
 #ifdef _WIN32
 #  define WIN32_LEAN_AND_MEAN
 #  include <windows.h>
+#  include <Mmsystem.h>
 
 #else
 #  include <time.h>
@@ -23,6 +28,7 @@
 HOBGOBLIN_NAMESPACE_BEGIN
 namespace util {
 
+using std::chrono::duration_cast;
 using std::chrono::milliseconds;
 using std::chrono::microseconds;
 
@@ -33,31 +39,58 @@ void Sleep(milliseconds timeToSleep) {
 #ifdef _WIN32
 // ****************** WINDOWS ******************
 
-void PreciseSleep(microseconds timeToSleep) {
-    ::LARGE_INTEGER ft;
-    ft.QuadPart = -static_cast<std::int64_t>(timeToSleep.count()) * 10; // '-' for using relative time
+namespace detail {
+void CALLBACK SignallingTimerCallback(UINT      timerID,
+                                      UINT      /* reserved */,
+                                      DWORD_PTR userData,
+                                      DWORD_PTR /* reserved */,
+                                      DWORD_PTR /* reserved */) {
+    static_assert(sizeof(DWORD_PTR) == sizeof(void*));
+    auto* sem = reinterpret_cast<Semaphore*>(userData);
+    sem->signal();
+}
+} // namespace detail
 
-    ::HANDLE timer = ::CreateWaitableTimer(NULL, TRUE, NULL);
-    ::SetWaitableTimer(timer, &ft, 0, NULL, NULL, FALSE);
-    ::WaitForSingleObject(timer, INFINITE);
-    ::CloseHandle(timer);
+void PreciseSleep(milliseconds timeToSleep) {
+    if (timeToSleep.count() <= 0) {
+        return;
+    }
+
+#ifndef NDEBUG
+    TIMECAPS tc;
+    if (timeGetDevCaps(&tc, sizeof(TIMECAPS)) != TIMERR_NOERROR) {
+        assert(false && "Win32 API call 'timeGetDevCaps()' failed");
+    }
+    assert(tc.wPeriodMin == 1);
+#endif // !NDEBUG
+
+    Semaphore sem{0};
+    auto mmresult = timeSetEvent(timeToSleep.count(),
+                                 1 /* minimal resolution in milliseconds */,
+                                 &detail::SignallingTimerCallback,
+                                 reinterpret_cast<DWORD_PTR>(&sem),
+                                 TIME_CALLBACK_FUNCTION);
+    if (mmresult == NULL) {
+        throw TracedRuntimeError{"Windows Multimedia timer could not be initialized"};
+    }
+    sem.wait();
 }
 
 #else
 // ****************** UNIX ******************
 
-void PreciseSleep(microseconds timeToSleep) {
+void PreciseSleep(milliseconds timeToSleep) {
     struct timespec ts;
-    ts.tv_sec  = static_cast<decltype(ts.tv_sec)>(timeToSleep.count() / 1'000'000);
-    ts.tv_nsec = static_cast<decltype(ts.tv_nsec)>(timeToSleep.count() % 1'000'000 * 1000);
+    ts.tv_sec  = static_cast<decltype(ts.tv_sec)>(timeToSleep.count() / 1'000);
+    ts.tv_nsec = static_cast<decltype(ts.tv_nsec)>((timeToSleep.count() % 1'000) * 1000);
 
-    while (nanosleep(&ts, &ts) == -1 && errno == EINTR);
+    while (nanosleep(&ts, &ts) == -1 && errno == EINTR) {}
 }
 
 #endif
 
 void SuperPreciseSleep(microseconds timeToSleep) {
-    if (timeToSleep.count() == 0) {
+    if (timeToSleep.count() <= 0) {
         return;
     }
 
@@ -66,10 +99,15 @@ void SuperPreciseSleep(microseconds timeToSleep) {
     Stopwatch stopwatch{};
 
     if (timeToSleep > PRECISE_SLEEP_GRANULARITY) {
-        PreciseSleep(timeToSleep - PRECISE_SLEEP_GRANULARITY);
+        PreciseSleep(duration_cast<milliseconds>(timeToSleep - PRECISE_SLEEP_GRANULARITY));
     }
 
-    while (stopwatch.getElapsedTime<std::chrono::microseconds>() < timeToSleep);
+    while (stopwatch.getElapsedTime<std::chrono::microseconds>() < timeToSleep) {
+        // Busy wait for the remainder of the time. yield() provides a hint to the
+        // implementation to reschedule the execution of threads, allowing other
+        // threads to run.
+        std::this_thread::yield();
+    }
 }
 
 } // namespace util
