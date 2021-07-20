38e24d9025d3b9ead426709af5e6fd00dacca4c5
Jovan Batnozic <jovanbatnozic@hotmail.rs>
2021-07-20 21:48:08 +0200

    Virtual Network Provider skeleton

---
+++ EngineCore/Modules/RigelNet/Include/Hobgoblin/RigelNet/Virtual_network_providers.hpp
@@ -0,0 +1,52 @@
+#ifndef UHOBGOBLIN_RN_VIRTUAL_NETWORK_PROVIDERS_HPP
+#define UHOBGOBLIN_RN_VIRTUAL_NETWORK_PROVIDERS_HPP
+
+#include <Hobgoblin/Private/Pmacro_define.hpp>
+
+#include <memory>
+
+#define HOBGOBLIN_RN_ZEROTIER_SUPPORT // TODO temp.
+#ifdef HOBGOBLIN_RN_ZEROTIER_SUPPORT
+#include <ZTCpp/Events.hpp>
+#include <ZTCpp/Ip_address.hpp>
+#endif
+
+HOBGOBLIN_NAMESPACE_BEGIN
+namespace rn {
+
+enum class RN_VirtualNetworkProviderType {
+    ZeroTier = 0 //! https://github.com/zerotier/libzt
+};
+
+class RN_VirtualNetworkProviderInterface {
+public:
+    //! Calls shutDown() automatically on destruction if it wasn't called
+    //! already before.
+    virtual ~RN_VirtualNetworkProviderInterface() = 0;
+
+    //! Returns the type of the virtual network provider.
+    virtual RN_VirtualNetworkProviderType getType() = 0;
+
+    //! TODO(add comment)
+    virtual void shutDown() = 0;
+
+    //! If this is set to true (it's always false by default), shutting
+    //! down the virtual network provider will also free all resources
+    //! it needs to function. If you do this, you won't be able to start
+    //! a new virtual network provider of the same type without restarting 
+    //! the whole application.
+    //! For some providers this may not be applicable, in which case it
+    //! is a no-op.
+    virtual void setWipeDataOnShutDown(bool aWipeDataOnShutDown) = 0;
+
+    // TODO(update config)
+};
+
+std::unique_ptr<RN_VirtualNetworkProviderInterface> RN_StartVirtualNetworkProvider();
+
+} // namespace rn
+HOBGOBLIN_NAMESPACE_END
+
+#include <Hobgoblin/Private/Pmacro_undef.hpp>
+
+#endif // !UHOBGOBLIN_RN_VIRTUAL_NETWORK_PROVIDERS_HPP
---
+++ EngineCore/Modules/RigelNet/Source/Virtual_network_providers.cpp
@@ -0,0 +1,14 @@
+
+#include <Hobgoblin/RigelNet/Virtual_network_providers.hpp>
+
+#include <Hobgoblin/Private/Pmacro_define.hpp>
+
+HOBGOBLIN_NAMESPACE_BEGIN
+namespace rn {
+
+int i = HOBGOBLIN_RN_ZEROTIER_SUPPORT;
+
+} // namespace rn
+HOBGOBLIN_NAMESPACE_END
+
+#include <Hobgoblin/Private/Pmacro_undef.hpp>
