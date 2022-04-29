38a2efffe30bfffacf4237b79eb6065be00a5ea4
Jovan Batnozic <jovanbatnozic@hotmail.rs>
2022-04-29 19:13:43 +0200

    Upgrade to ztcpp 2.1.0

--- EngineCore/Conan_install.bat
+++ EngineCore/Conan_install.bat
@@ -1,7 +1,7 @@
 @ECHO OFF
 
 REM SFML implementation used std::auto_ptr (which was removed in C++17)
-REM so we have to build it as C++14 unline the rest of the project.
+REM so we have to build it as C++14 unlike the rest of the project.
 REM Luckily, it's ABI-compatible with C++17.
 
 @ECHO ON
