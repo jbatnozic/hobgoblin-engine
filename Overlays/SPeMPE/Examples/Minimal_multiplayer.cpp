a142d340387d03ac23b0ebedbfc65cbb6fb8cd5e
Jovan Batnozic <jovanbatnozic@hotmail.rs>
2022-05-18 20:43:14 +0200

    More work

--- Overlays/SPeMPE/Examples/Minimal_multiplayer.cpp
+++ Overlays/SPeMPE/Examples/Minimal_multiplayer.cpp
@@ -430,7 +430,7 @@ void GameplayManager::_eventUpdate() {
 
 #define WINDOW_WIDTH           800
 #define WINDOW_HEIGHT          800
-#define FRAMERATE               30
+#define FRAMERATE               60
 
 bool MyRetransmitPredicate(hg::PZInteger aCyclesSinceLastTransmit,
                            std::chrono::microseconds aTimeSinceLastSend,
