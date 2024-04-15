0734fa53ddf33bc3277d15f68723f2397119fb17
Jovan Batnožić <Jovan.Batnozic@tomtom.com>
2024-04-15 19:43:36 +0200

    Add Conan wrapper

---
+++ conanw.bash
@@ -0,0 +1,62 @@
+#! /bin/bash
+
+echo "Looking for conan..."
+if ! command -v git &> /dev/null
+then
+    echo "Error: conan could not be found; exiting."
+    exit 1
+fi
+echo "conan found"
+
+if [[ "$1" == "clean" ]];
+then
+    # No extra arguments required
+
+    rm -rf build_
+
+    exit 0
+fi
+
+if [[ "$1" == "install" ]];
+then
+    # 2nd argument: compiler (gcc|clang)
+    # 3rd argument: profile  (Debug|Release)
+
+    conan install . -of build_/ \
+        -pr:h=hobgoblin-$2 \
+        -pr:b=hobgoblin-$2 \
+        -s build_type=$3 \
+        --build=missing \
+        --build=outdated \
+        --build=cascade
+
+    exit 0
+fi
+
+if [[ "$1" == "build" ]];
+then
+    # 2nd argument: compiler (gcc|clang|apple-clang)
+    # 3rd argument: profile  (Debug|Release)
+
+    conan build . -of build_/ \
+        -pr:h=hobgoblin-$2 \
+        -pr:b=hobgoblin-$2 \
+        -s build_type=$3 \
+        --build=missing \
+        --build=outdated \
+        --build=cascade
+
+    exit 0
+fi
+
+if [[ "$1" == "export" ]];
+then
+    # No extra arguments required
+
+    conan export . --user jbatnozic --channel stable
+
+    exit 0
+fi
+
+echo "Error: unknown command '$1' provided; exiting."
+exit 1
