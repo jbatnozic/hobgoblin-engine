02cd5a0b7c29f5862518e71be3bf419baabf7630
Jovan Batnožić <10475373+jbatnozic@users.noreply.github.com>
2024-08-02 01:00:11 +0200

    Intake RmlUi 5.1

--- Scripts/Get_conan_dependencies.bash
+++ Scripts/Get_conan_dependencies.bash
@@ -6,6 +6,9 @@ LIBZT_VERSION=3.0.2
 ZTCPP_COMMITHASH=bb4c8d25ba8dbcea783f5e742066ce943344f762
 ZTCPP_VERSION=3.0.2
 
+RMLUI_COMMITHASH=33dc5517c883edf9bf54f9339bf223333e8f79c4
+RMLUI_VERSION=5.1
+
 echo "Looking for git..."
 if ! command -v git &> /dev/null
 then
@@ -38,6 +41,14 @@ git clone https://github.com/jbatnozic/ztcpp
 pushd ztcpp
 git checkout ${ZTCPP_COMMITHASH}
 conan export . --version ${ZTCPP_VERSION} --user jbatnozic --channel stable
+# TODO: exit on failure of conan export
+popd
+
+echo "Getting RmlUi..."
+git clone https://github.com/jbatnozic/rmlui-conan
+pushd rmlui-conan
+git checkout ${RMLUI_COMMITHASH}
+conan export . --version ${RMLUI_VERSION} --user jbatnozic --channel stable
 # TODO: exit on failure of conan export
 popd
 
