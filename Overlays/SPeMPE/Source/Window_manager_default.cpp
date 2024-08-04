c1c3a7e6051ac66e36727239a139703f18322026
Jovan Batnožić <10475373+jbatnozic@users.noreply.github.com>
2024-08-05 01:12:23 +0200

    Remove log I dont need

--- Overlays/SPeMPE/Source/Window_manager_default.cpp
+++ Overlays/SPeMPE/Source/Window_manager_default.cpp
@@ -4,7 +4,6 @@
 #include <SPeMPE/Managers/Window_manager_default.hpp>
 
 #include <Hobgoblin/Common.hpp>
-#include <Hobgoblin/Logging.hpp>
 #include <Hobgoblin/HGExcept.hpp>
 #include <Hobgoblin/Math/Vector.hpp>
 #include <Hobgoblin/Window.hpp>
